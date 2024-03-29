f23e8a73f339610928c664a467c71396b49f6398
codetuner <codetuner@users.noreply.github.com>
2024-03-29 13:54:55 +0100

    JsonConvert default serializer options.

--- Arebis.Core.EntityFramework/ValueConversion/JsonConverter.cs
+++ Arebis.Core.EntityFramework/ValueConversion/JsonConverter.cs
@@ -4,24 +4,37 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Arebis.Core.EntityFramework.ValueConversion
 {
+    /// <summary>
+    /// Non-generic JsonConverter class holding serializer options.
+    /// </summary>
+    public abstract class JsonConverter
+    {
+        /// <summary>
+        /// Options to use when (de)serializing values.
+        /// </summary>
+        public static JsonSerializerOptions? SerializerOptions { get; set; } = new()
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+            AllowTrailingCommas = true,
+            IgnoreReadOnlyFields = true,
+            IgnoreReadOnlyProperties = true,            
+        };
+    }
+
     /// <summary>
     /// A value converter that stores objects as Json.
     /// </summary>
     public class JsonConverter<T> : ValueConverter<T, string?>
     {
-        /// <summary>
-        /// Options to use when (de)serializing values.
-        /// </summary>
-        public static JsonSerializerOptions? SerializerOptions { get; set; }
-
         /// <inheritdoc/>
         public JsonConverter()
             : base(
-                  value => JsonSerializer.Serialize(value, SerializerOptions),
-                  value => JsonSerializer.Deserialize<T>(value ?? "null", SerializerOptions)!,
+                  value => JsonSerializer.Serialize(value, JsonConverter.SerializerOptions),
+                  value => JsonSerializer.Deserialize<T>(value ?? "null", JsonConverter.SerializerOptions)!,
                   null)
         { }
     }
