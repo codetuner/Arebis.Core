ac281ff2677f15b943f4c5314c6da96e27f0ee07
codetuner <codetuner@users.noreply.github.com>
2024-04-15 11:53:42 +0200

    Arebis.Core.EntityFramework.SqlServer with AddScopedSqlDbContext extension method to share SqlServer connections using a SqlConnectionSource.

---
+++ Arebis.Core.EntityFramework.SqlServer/README.md
@@ -0,0 +1,6 @@
+Arebis.Core.EntityFramework.SqlServer
+=====================================
+
+.NET Core extensions for working with Entity Framework using SQLServer data source..
+
+
---
+++ Arebis.Core.EntityFramework.SqlServer/ServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Arebis.Core.Data.Sql;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arebis.Core.EntityFramework.SqlServer
+{
+    /// <summary>
+    /// Extensions to service registrations.
+    /// </summary>
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// Adds a scoped DbContext using SqlServer and sharing connections through a registered SqlConnectionSource service.
+        /// Use <code>builder.Services.AddScoped&lt;SqlConnectionSource&gt;();</code> to register a SqlConnectionSource.
+        /// </summary>
+        public static void AddScopedSqlDbContext<TDbContext> (this IServiceCollection services, string connectionName = "DefaultConnection", Action<DbContextOptionsBuilder<TDbContext>>? optionsAction = null)
+            where TDbContext : DbContext
+        {
+            services.AddScoped<DbContextOptions<TDbContext>>(serviceProvider =>
+            {
+                var builder = new DbContextOptionsBuilder<TDbContext>();
+                var source = serviceProvider.GetRequiredService<SqlConnectionSource>();
+                var connection = source[connectionName];
+                builder.UseSqlServer(connection);
+                optionsAction?.Invoke(builder);
+
+                return builder
+                    .Options;
+            });
+
+            services.AddScoped<TDbContext>();
+        }
+    }
+}
