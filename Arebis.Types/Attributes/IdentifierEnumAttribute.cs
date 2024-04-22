7941ae823a0a722a3ea2f5d664ca7fabd1f946c0
codetuner <codetuner@users.noreply.github.com>
2024-04-22 23:23:38 +0200

    Added IdentifierEnumAttribute

---
+++ Arebis.Types/Attributes/IdentifierEnumAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arebis.Types.Attributes
+{
+    /// <summary>
+    /// Marks an enum as identifier, allowing it to have any value even undefined ones.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Enum)]
+    public class IdentifierEnumAttribute : Attribute
+    { }
+}
