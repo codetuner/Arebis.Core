9ebabe001a4b5a503c22a58b005e73da43a6342a
codetuner <rudi@breedenraedt.be>
2025-08-01 19:07:29 +0200

    FileSystemExtensionsTests

---
+++ Arebis.Core.Tests/Extensions/FileSystemExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using Arebis.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arebis.Core.Tests.Extensions
+{
+    [TestClass]
+    public class FileSystemExtensionsTests
+    {
+        [TestMethod]
+        public void ToUniqueName_FileExists_ReturnsUniqueFileName()
+        {
+            Console.WriteLine("Test to run interactively...");
+            //Console.WriteLine(Environment.CurrentDirectory);
+            //Console.WriteLine(((FileInfo?)null).ToUniqueName());
+            //Console.WriteLine(new FileInfo("testhost.dll").ToUniqueName());
+            //Console.WriteLine(new FileInfo("Arebis.Core.xml").ToUniqueName());
+            //Console.WriteLine(new FileInfo("test").ToUniqueName());
+            //Console.WriteLine(new FileInfo(".txt").ToUniqueName());
+            //Console.WriteLine(new FileInfo(@"C:\Users\RudiBreedenraedt\Downloads\20250729 Kostenvoranschlag, Rechnung und Gutschrift - with markings.pdf").ToUniqueName());
+            //Console.WriteLine(new FileInfo(@"C:\Users\RudiBreedenraedt\Downloads\Gastello High Point pdf.pdf").ToUniqueName());
+            //Console.WriteLine(new FileInfo(@"C:\Users\RudiBreedenraedt\Downloads\Foobar.txt").ToUniqueName());
+            //Console.WriteLine(new FileInfo(@"C:\Users\RudiBreedenraedt\Downloads\20250730 SDWorx Academy - mobiliteitsbudget.pdf").ToUniqueName());
+        }
+    }
+}
