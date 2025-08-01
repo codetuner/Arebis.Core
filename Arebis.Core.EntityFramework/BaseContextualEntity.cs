9ec805d2b8a1a140a4100c0b63ec54b3de985d1b
codetuner <rudi@breedenraedt.be>
2025-08-01 19:08:47 +0200

    Added BaseContextualEntity and BaseReadOnlyEntity.

---
+++ Arebis.Core.EntityFramework/BaseContextualEntity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arebis.Core.EntityFramework
+{
+    /// <summary>
+    /// Abstract base entity class that implements <see cref="IContextualEntity{TContext}"/> to provide access to the context.
+    /// </summary>
+    public abstract class BaseContextualEntity<TContext> : IContextualEntity<TContext>
+        where TContext : DbContext
+    {
+        /// <inheritdoc/>
+        public TContext? Context { get; set; }
+    }
+}
---
+++ Arebis.Core.EntityFramework/BaseReadOnlyEntity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Arebis.Core.EntityFramework
+{
+    /// <summary>
+    /// A base class for read-only entities that are associated with a specific DbContext.
+    /// Prevents saving changes to the database.
+    /// </summary>
+    public abstract class BaseReadOnlyEntity : IInterceptingEntity
+    {
+        /// <inheritdoc />
+        public bool OnSaving(EntityEntry entityEntry)
+        {
+            if (entityEntry.State == EntityState.Added || entityEntry.State == EntityState.Modified || entityEntry.State == EntityState.Deleted)
+            {
+                throw new InvalidOperationException("Read-only entities cannot be saved to the database.");
+            }
+            else
+            {
+                return true; // Allow other operations like querying
+            }
+        }
+    }
+
+    /// <summary>
+    /// A base class for read-only entities that are associated with a specific DbContext.
+    /// Prevents saving changes to the database.
+    /// Implements <see cref="IContextualEntity{TContext}"/> to provide access to the context.
+    /// </summary>
+    /// <typeparam name="TContext">The context type the entity type belongs to.</typeparam>
+    public abstract class BaseReadOnlyEntity<TContext> : BaseReadOnlyEntity, IContextualEntity<TContext>
+        where TContext : DbContext
+    {
+        /// <inheritdoc />
+        public TContext? Context { get; set; }
+    }
+}
