1b0fbfe3cab12d7967f4c337753d6939f5df1652
codetuner <rudi@breedenraedt.be>
2025-10-28 12:38:02 +0100

    Added RegexValueConverter

---
+++ Arebis.Core.EntityFramework/ValueConversion/RegexValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Arebis.Core.EntityFramework.ValueConversion
+{
+    /// <summary>
+    /// A value converter that stores a Regex as string.
+    /// </summary>
+    public class RegexValueConverter : ValueConverter<Regex, string>
+    {
+        /// <inheritdoc/>
+        public RegexValueConverter()
+            : base(
+                value => value.ToString(),
+                value => new Regex(value),
+                null)
+        { }
+
+        /// <inheritdoc/>
+        public RegexValueConverter(RegexOptions options)
+            : base(
+                value => value.ToString(),
+                value => new Regex(value, options),
+                null)
+        { }
+    }
+}
