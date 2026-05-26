4c0fbe9d987ffc86021ef1816f85afae07c9d96b
codetuner <rudi@breedenraedt.be>
2026-05-26 18:25:29 +0200

    Added Client Hints support.

--- Arebis.Core.AspNet/ApplicationBuilderExtensions.cs
+++ Arebis.Core.AspNet/ApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,27 @@ namespace Arebis.Core.AspNet
     /// </summary>
     public static class ApplicationBuilderExtensions
     {
+        /// <summary>
+        /// Requests for client hints.
+        /// </summary>
+        /// <param name="app">The app to configure.</param>
+        /// <param name="aspects">The client hint aspects to request (incl. citical ones, i.e. "Sec-CH-UA-Mobile, Sec-CH-Viewport-Width, Sec-CH-Viewport-Height, Sec-CH-Prefers-Reduced-Motion"). Defaults to "Sec-CH-UA-Mobile, Sec-CH-Viewport-Width".</param>
+        /// <param name="criticalAspects">The critical client hint aspects to request, if any (i.e. "Sec-CH-Prefers-Reduced-Motion").</param>
+        public static IApplicationBuilder UseClientHints(this IApplicationBuilder app, string aspects = "Sec-CH-UA-Mobile, Sec-CH-Viewport-Width", string? criticalAspects = null)
+        {
+            return app.Use(async (context, next) =>
+            {
+                context.Response.OnStarting(() =>
+                {
+                    if (!String.IsNullOrWhiteSpace(aspects)) context.Response.Headers.Append("Accept-CH", aspects);
+                    if (!String.IsNullOrWhiteSpace(criticalAspects)) context.Response.Headers.Append("Critical-CH", criticalAspects);
+                    return Task.CompletedTask;
+                });
+
+                await next();
+            });
+        }
+
         /// <summary>
         /// Installs support for HttpResponseException exceptions using middleware.
         /// </summary>
---
+++ Arebis.Core.AspNet/ContextExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arebis.Core.AspNet
+{
+    /// <summary>
+    /// Context extensions.
+    /// </summary>
+    public static class ContextExtensions
+    {
+        /// <summary>
+        /// Get client hints.
+        /// </summary>
+        /// <example>
+        /// <code>string? mobileHint = context.GetClientHint("Sec-CH-UA-Mobile");</code>
+        /// </example>
+        public static string? GetClientHint(this Microsoft.AspNetCore.Http.HttpContext context, string hintName)
+        {
+            if (context.Request.Headers.TryGetValue(hintName, out var hintValue))
+                return hintValue.ToString();
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// Whether the viewport is narrow (e.g. mobile) based on client hints.
+        /// </summary>
+        /// <param name="context">The HTTP context.</param>
+        /// <param name="narrowWidth">The maximum width considered narrow.</param>
+        /// <returns>True if the viewport is narrow, otherwise false.</returns>
+        public static bool IsViewportNarrow(this Microsoft.AspNetCore.Http.HttpContext context, int narrowWidth = 767)
+        {
+            var widthHint = context.GetClientHint("Sec-CH-Viewport-Width");
+            if (widthHint != null && int.TryParse(widthHint, out var widthValue))
+                return widthValue <= narrowWidth;
+
+            var mobileHint = context.GetClientHint("Sec-CH-UA-Mobile");
+            if (mobileHint != null)
+                return mobileHint == "?1";
+
+            return false;
+        }
+    }
+}
